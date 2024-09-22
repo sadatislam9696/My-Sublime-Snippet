75a862a6678ba32f6578e62412462ce2a8ee1c94
sadat islam <pathchari5@gmail.com>
2024-09-22 11:41:56 +0600

    first commit

---
+++ Template.cpp
@@ -0,0 +1,46 @@
+/***   Bismillahir Rahmanir Rahim   ***/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define     nn          '\n'
+#define     ss          second
+#define     ff          first
+#define     sp(d, n)    fixed << setprecision(d) << n
+#define     srt(v)      sort(v.begin(),v.end());
+#define     rsrt(v)     sort(v.rbegin(),v.rend());
+#define     rev(v)      reverse(v.begin(),v.end());
+#define     mem(a, x)   memset(a, x, sizeof a)
+
+using ll =  long long;
+using ld =  long double;
+using ull = unsigned long long;
+const ld pi = acos(-1.0);
+template <typename T>
+using minHeap = priority_queue<T, vector<T>, greater<T>>;
+
+int dx[6] = {0, 1, -1, 0, 0};
+int dy[6] = {0, 0, 0, 1, -1};
+
+const int M = 1e9 + 7;
+const int N = 1e3 + 9;
+
+void solution(){
+
+}
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);cout.tie(nullptr);
+
+    int t = 1, c = 1;
+    cin >> t;
+    //cin.ignore();
+    while(t--){
+        //cout << "Case " << c++ << ": ";
+        solution();
+    }
+    return 0;
+}
+
+
